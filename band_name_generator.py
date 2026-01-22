322779191e48056519ac5ba6d1b98015a226b49c
C1nnam0n-dev <0715tshepomatima@gmail.com>
2026-01-22 16:49:22 +0200

    Create band_name_generator.py

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+print("Greetings future band members! Lets come up with a cool name for your band.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet you had in your childhood?\n")
+print("What do you think of " + city + " " + pet + "'s" + " as your band name?")
